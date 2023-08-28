2ef6b1aab39c044f22fa232f121c9286e68e4b45
Pedro Viana <Pedro Viana>
2023-08-28 19:08:21 -0300

    feat: class PessoaEleitora & PessoaCandidata

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,44 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos;
+
+  /**
+   * Construtor.
+   */
+  public PessoaCandidata(String name, int number) {
+    super.nome = name;
+    this.numero = number;
+    this.votos = 0;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String newName) {
+    nome = newName;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int newNumber) {
+    numero = newNumber;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int newValue) {
+    votos = newValue;
+  }
+
+  public void receberVoto() {
+    votos += 1;
+  }
 
 }
